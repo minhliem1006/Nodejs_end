4760778a83c91a87b01090b6406393c4068850b9
Fawker <81698715+minhliem1006@users.noreply.github.com>
2021-07-30 01:50:09 +0700

    change code handlebars

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const exphbs  = require('express-handlebars');
+const path = require ('path');
+const port = 3000;
 
+
+app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('home');
 })
+
+app.get('/news', (req, res) => {
+    res.render('news');
+  })
+app.get('/courses', (req, res) => {
+    res.render('courses');
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
